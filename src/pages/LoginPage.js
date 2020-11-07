5208f0b334232b48dc4b57e501955302f10e9305
matheuscarv69 <matheus9126@gmail.com>
2020-11-07 14:24:17 -0400

    LoginPage Fix

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,41 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import FormRow from "../components/FormRow";
-
 
 export default class LoginPage extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <StatusBar style="light" />
         <FormRow>
-          <TextInput />
+          <TextInput style={styles.input} placeholder="Email" />
         </FormRow>
         <FormRow>
-          <TextInput />
+          <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true}/>
         </FormRow>
+        <Text>Entrar</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    paddingLeft: 10,
+    paddingRight: 10,
+    
+    marginLeft: 20,
+    marginRight: 20,
+    
+    flex:1,
+    justifyContent: "center",
+    
+    
+  },
+  input: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5,
+  },
+});
