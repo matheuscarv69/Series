95ccf035597894f7606bb42e34a74d87714f7871
matheuscarv69 <matheus9126@gmail.com>
2020-11-24 16:05:29 -0400

    Creation onChangeHandler, ES6 method

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,15 +4,42 @@ import { StatusBar } from "expo-status-bar";
 import FormRow from "../components/FormRow";
 
 export default class LoginPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      mail: '',
+      password: '',
+    }
+
+  }
+
+  onChangeHandler(field, value){
+    this.setState({
+      [field]: value
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <StatusBar style="light" />
         <FormRow>
-          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput 
+          style={styles.input} 
+          placeholder="Email" 
+          value={this.state.mail}
+          onChangeText={value => this.onChangeHandler('mail',value)}
+          />
         </FormRow>
         <FormRow>
-          <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true}/>
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            secureTextEntry={true}
+            value={this.state.password}
+            onChangeText={value => this.onChangeHandler('password',value)}
+          />
         </FormRow>
         <Text>Entrar</Text>
       </View>
@@ -21,17 +48,15 @@ export default class LoginPage extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     paddingLeft: 10,
     paddingRight: 10,
-    
+
     marginLeft: 20,
     marginRight: 20,
-    
-    flex:1,
+
+    flex: 1,
     justifyContent: "center",
-    
-    
   },
   input: {
     paddingLeft: 5,
